dbfb881a450beffa92631afff9fa2f78c47d784c
srbalbucio <srbalbuciogf@gmail.com>
2023-07-23 16:51:04 -0300

    Adicionado overrides do H2 ao Hikari H2;

--- src/main/java/balbucio/sqlapi/h2/HikariH2Instance.java
+++ src/main/java/balbucio/sqlapi/h2/HikariH2Instance.java
@@ -1,8 +1,16 @@
 package balbucio.sqlapi.h2;
 
 import balbucio.sqlapi.hikari.HikariInstance;
+import balbucio.sqlapi.model.ConditionValue;
 import balbucio.sqlapi.sqlite.SqliteConfig;
 import com.zaxxer.hikari.HikariConfig;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HikariH2Instance extends HikariInstance {
 
@@ -23,5 +31,75 @@ public class HikariH2Instance extends HikariInstance {
         hk.setPassword(sqlconfig.getPassword());
         hk.setDriverClassName("org.h2.Driver");
         setHikariConfig(hk);
+    }
+
+    @Override
+    public Map<String, String> getColumns(String tableName) {
+        Map<String, String> values = new HashMap<>();
+        try{
+            if(!isConnected()){
+                connect();
+            }
+
+            Statement statement = getStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM "+tableName);
+            ResultSetMetaData metaData = resultSet.getMetaData();
+
+            int numeroColunas = metaData.getColumnCount();
+
+            for (int i = 1; i <= numeroColunas; i++) {
+                values.put(metaData.getColumnName(i), metaData.getColumnTypeName(i));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return values;
+    }
+
+    @Override
+    public boolean exists(ConditionValue[] values, String tableName) {
+        boolean exist = false;
+        try{
+            Statement statement = getStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM " + tableName + " WHERE "+getConditionQuery(values));
+            resultSet.next();
+            int count = resultSet.getInt(1);
+            exist = count>0;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return exist;
+    }
+
+    @Override
+    public boolean exists(ConditionValue value, String tableName) {
+        boolean exist = false;
+        try{
+            PreparedStatement statement = getPreparedStatement("SELECT COUNT(*) FROM " + tableName + " WHERE "+value.getColumn()+" "+value.getConditional().getValue()+" ?");
+            statement.setObject(1, value.getValue());
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            int count = resultSet.getInt(1);
+            exist = count>0;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return exist;
+    }
+
+    @Override
+    public boolean exists(String column, String condition, Object item, String tableName) {
+        boolean exist = false;
+        try{
+            PreparedStatement statement = getPreparedStatement("SELECT COUNT(*) FROM " + tableName + " WHERE " +column+ " "+condition+" ?");
+            statement.setObject(1, item);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            int count = resultSet.getInt(1);
+            exist = count>0;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return exist;
     }
 }
