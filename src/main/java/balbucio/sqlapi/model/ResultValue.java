b8b5aa4eb13742d27d452f3a2940626b551218a8
srbalbucio <srbalbuciogf@gmail.com>
2023-07-23 13:22:53 -0300

    Adicionado getter ao ResultValue;

--- src/main/java/balbucio/sqlapi/model/ResultValue.java
+++ src/main/java/balbucio/sqlapi/model/ResultValue.java
@@ -16,4 +16,27 @@ public class ResultValue {
     private String tableName;
     private Map<String, Object> valueByColumn = new HashMap<>();
 
+    public boolean has(String column){
+        return valueByColumn.containsKey(column);
+    }
+    public Object get(String column){
+        return valueByColumn.get(column);
+    }
+
+    public String asString(String column){
+        return (String) get(column);
+    }
+
+    public Boolean asBool(String column){
+        return (boolean) get(column);
+    }
+
+    public int asInt(String column){
+        return (int) get(column);
+    }
+
+    public long asLong(String column){
+        return (long) get(column);
+    }
+
 }
